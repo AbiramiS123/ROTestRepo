6b84de55202fc2c27c1b3a8fa589a31ccda5c4e6
Abirami <120541663+AbiramiS123@users.noreply.github.com>
2023-02-16 12:47:09 +0530

    Update main.py
    added WSGI Server instead of development server

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
-
+from gevent.pywsgi import WSGIServer
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
